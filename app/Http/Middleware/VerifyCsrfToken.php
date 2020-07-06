7929bd6419961e383854f7cbbc4f282a0156e048
Vinicius Bassalobre <=>
2020-07-06 16:04:49 -0300

    add exceção para verificação de token na api do dashboard

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -20,6 +20,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         "admin/inputs/option_list",
-        "admin/dashboard/get_info"
+        "admin/dashboard/get_info/*"
     ];
 }

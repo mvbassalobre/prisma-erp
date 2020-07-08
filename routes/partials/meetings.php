6f5be75575b8cdfaec552e07756884ef3f693878
Luiz Pontes <luizpontes@otimize.me>
2020-07-08 09:29:01 -0300

    adiciona arquivos de email markdown

--- routes/partials/meetings.php
+++ routes/partials/meetings.php
@@ -6,6 +6,7 @@ Route::prefix('meetings')
         Route::post('create', 'MeetingController@save');
         Route::get('{meeting}/edit', "MeetingController@edit")->middleware(["hashids:meeting", "bindings"]);
         Route::post('{meeting}/edit', "MeetingController@save")->middleware(["hashids:meeting", "bindings"]);
+        Route::get('debuug', "MeetingController@debuug");
         //Route::get('{meeting}', "MeetingController@show")->middleware(["hashids:meeting", "bindings"]);
         //Route::get('createe', 'MeetingController@create');
     });

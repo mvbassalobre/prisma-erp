f23aa613df13af8b9baf310a48081b6eab29b1df
Vinicius Bassalobre <=>
2020-10-20 16:42:26 -0300

    corrige erro cadastro cliente

--- app/Http/Models/Customer.php
+++ app/Http/Models/Customer.php
@@ -9,131 +9,138 @@ use Carbon\Carbon;
 use marcusvbda\vstack\Models\Scopes\TenantScope;
 use marcusvbda\vstack\Models\Observers\TenantObserver;
 use Auth;
+use App\User;
 
 class Customer extends DefaultModel
 {
-    protected $table = "customers";
-    public $cascadeDeletes = ["sales"];
-    // public $restrictDeletes = [];
-    protected $appends = ['code', 'f_created_at', 'last_update', 'phones', 'actions', 'team_name','f_user'];
+	protected $table = "customers";
+	public $cascadeDeletes = ["sales"];
+	// public $restrictDeletes = [];
+	protected $appends = ['code', 'f_created_at', 'last_update', 'phones', 'actions', 'team_name', 'f_user'];
 
-    public $casts = [
-        "timeline" => "Array",
-        "data" => "Array",
-    ];
+	public $casts = [
+		"timeline" => "Array",
+		"data" => "Array",
+	];
 
-    public static function hasTenant()
-    {
-        return false;
-    }
+	public static function hasTenant()
+	{
+		return false;
+	}
 
-    public function getFUserAttribute()
-    {
-        $user = $this->user;
-        return "<a href='/admin/users/" . $user->code . "'>" . $user->name . "</a>";
-    }
+	public function setUserIdAttribute($value)
+	{
+		$user = User::where("name", $value)->first();
+		$this->attributes["user_id"] = @$user->id ? $user->id : Auth::user()->id;
+	}
 
-    public static function boot()
-    {
-        $user = Auth::user();
-        parent::boot();
-        if (Auth::check()) {
-            if ($user->hasRole(["admin", "user"])) {
-                static::observe(new TenantObserver());
-                static::addGlobalScope(new TenantScope());
-            }
-        }
-        self::creating(function ($model) use ($user) {
-            $user = $user ? $user->name : "root";
-            $model->timeline = [[
-                "title" => "Cadastro",
-                "description" => "Cadastrado no sistema por <b>$user</b>",
-                "datetime" => Carbon::now()->format('d/m/Y - H:i:s'),
-            ]];
-        });
+	public function getFUserAttribute()
+	{
+		$user = $this->user;
+		return "<a href='/admin/users/" . $user->code . "'>" . $user->name . "</a>";
+	}
 
-        if (!$user) return;
-        if ($user->hasRole(["super-admin", "admin"])) return;
-        static::observe(new UserObserver());
-        static::addGlobalScope(new UserScope());
-    }
+	public static function boot()
+	{
+		$user = Auth::user();
+		parent::boot();
+		if (Auth::check()) {
+			if ($user->hasRole(["admin", "user"])) {
+				static::observe(new TenantObserver());
+				static::addGlobalScope(new TenantScope());
+			}
+		}
+		self::creating(function ($model) use ($user) {
+			$user = $user ? $user->name : "root";
+			$model->timeline = [[
+				"title" => "Cadastro",
+				"description" => "Cadastrado no sistema por <b>$user</b>",
+				"datetime" => Carbon::now()->format('d/m/Y - H:i:s'),
+			]];
+		});
 
-    public function appendToTimeline($title, $desc, $tags = [])
-    {
-        $timeline = @$this->timeline ? (is_array($this->timeline) ? $this->timeline : []) : [];
-        array_unshift($timeline, [
-            "title" => $title,
-            "description" => $desc,
-            "datetime" => Carbon::now()->format('d/m/Y - H:i:s')
-        ]);
-        $this->timeline = $timeline;
-        $this->save();
-    }
+		if (!$user) return;
+		if ($user->hasRole(["super-admin", "admin"])) return;
+		static::observe(new UserObserver());
+		static::addGlobalScope(new UserScope());
+	}
 
-    public function getLastUpdateAttribute()
-    {
-        if (!$this->updated_at) return;
-        return $this->updated_at->diffForHumans();
-    }
+	public function appendToTimeline($title, $desc, $tags = [])
+	{
+		$timeline = @$this->timeline ? (is_array($this->timeline) ? $this->timeline : []) : [];
+		array_unshift($timeline, [
+			"title" => $title,
+			"description" => $desc,
+			"datetime" => Carbon::now()->format('d/m/Y - H:i:s')
+		]);
+		$this->timeline = $timeline;
+		$this->save();
+	}
 
-    public function getActionsAttribute()
-    {
-        return "<div class='d-flex flex-column'>
+	public function getLastUpdateAttribute()
+	{
+		if (!$this->updated_at) return;
+		return $this->updated_at->diffForHumans();
+	}
+
+	public function getActionsAttribute()
+	{
+		return "<div class='d-flex flex-column'>
                     <div>Atualizado " . $this->last_update . "</div>
                     <a href='" . route('admin.customers.attendance.index', ['code' => $this->code]) . "' class='crud-btns link d-flex align-items-center'>
                         <i class='el-icon-s-finance mr-1'></i> Atendimento
                     </a>
                 </div>";
-    }
+	}
 
-    public function tenant()
-    {
-        return $this->belongsTo(Tenant::class);
-    }
+	public function tenant()
+	{
+		return $this->belongsTo(Tenant::class);
+	}
 
-    public function meetings()
-    {
-        return $this->hasMany(Meeting::class);
-    }
+	public function meetings()
+	{
+		return $this->hasMany(Meeting::class);
+	}
 
-    public function getFCreatedAtAttribute()
-    {
-        if (!$this->created_at) return;
-        return @$this->created_at->format("d/m/Y - H:i:s");
-    }
+	public function getFCreatedAtAttribute()
+	{
+		if (!$this->created_at) return;
+		return @$this->created_at->format("d/m/Y - H:i:s");
+	}
 
-    public function getPhonesAttribute()
-    {
-        return "<p class='mb-0'>" . $this->phone . "</p><p class='mb-0'>" . $this->cellphone . "</p>";
-    }
+	public function getPhonesAttribute()
+	{
+		return "<p class='mb-0'>" . $this->phone . "</p><p class='mb-0'>" . $this->cellphone . "</p>";
+	}
 
-    public function gender()
-    {
-        return $this->belongsTo(Gender::class);
-    }
+	public function gender()
+	{
+		return $this->belongsTo(Gender::class);
+	}
 
-    public function maritalStatus()
-    {
-        return $this->belongsTo(MaritalStatus::class);
-    }
+	public function maritalStatus()
+	{
+		return $this->belongsTo(MaritalStatus::class);
+	}
 
-    public function bank()
-    {
-        return $this->belongsTo(\Bank::class);
-    }
+	public function bank()
+	{
+		return $this->belongsTo(\Bank::class);
+	}
 
-    public function user()
-    {
-        return $this->belongsTo(\App\User::class);
-    }
+	public function user()
+	{
+		return $this->belongsTo(\App\User::class);
+	}
 
-    public function sales()
-    {
-        return $this->hasMany(Sale::class);
-    }
+	public function sales()
+	{
+		return $this->hasMany(Sale::class);
+	}
 
-    public function getTeamNameAttribute()
-    {
-        return @$this->user->team->name;
-    }
+	public function getTeamNameAttribute()
+	{
+		return @$this->user->team->name;
+	}
 }
